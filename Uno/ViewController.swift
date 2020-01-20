5673941a5b8d2979b8d3ffaccfcfdcb6b35ca659
Aman Ketebo <amanuel.ketebo@willowtreeapps.com>
2020-01-20 18:44:30 -0500

    Add project

---
+++ Uno/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Uno
+//
+//  Created by Amanuel Ketebo on 1/20/20.
+//  Copyright © 2020 com.sincitydev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
